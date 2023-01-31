8310b299c5bb4829ff407f116bca387e4c2a9803
skyWrightC <corey.wright@sky.uk>
2023-01-31 10:23:51 +0000

    Strings as arrays

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -13,12 +13,17 @@ public class Main {
 
         //System.out.println(oneToTen[999]); //there is no index at 999 so it breaks
 
-        String [] names = {"Corey", "Jamie", "Steve"};
+        String[] names = {"Corey", "Jamie", "Steve"};
 
-        for (int i = 0; i < names.length; i++){
-            System.out.println(i +":" + names[i]);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(i + ":" + names[i]);
         }
-        
+
         for (String name : names) System.out.println(name);
+
+
+    //    strings As char arrays accessing the first character.
+    String name = "Corey";
+    System.out.println(name.charAt(0));
     }
 }
