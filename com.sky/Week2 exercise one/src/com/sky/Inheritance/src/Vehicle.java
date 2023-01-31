e58748781922e62d7bd508a736df5f5a34b4b5c1
skyWrightC <corey.wright@sky.uk>
2023-01-31 16:03:58 +0000

    refactored folders

---
+++ com.sky/Week2 exercise one/src/com/sky/Inheritance/src/Vehicle.java
@@ -0,0 +1,21 @@
+
+public class Vehicle{
+
+        String make;
+        String model;
+        String colour;
+
+        int numbers;
+
+        void refuel(){
+
+        }
+
+        void accelerate(){
+
+        }
+
+        void brake(){
+
+        }
+    }
